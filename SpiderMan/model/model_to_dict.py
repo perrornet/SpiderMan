fc56b99b78ed0c5b1a697c50ab198c768e17aa29
perror <15058342792@163.com>
2018-03-19 16:54:58 +0800

    Add the peewee serialization module

--- SpiderMan/model/model_to_dict.py
+++ SpiderMan/model/model_to_dict.py
@@ -1,4 +1,109 @@
 # -*- coding:utf-8 -*-
+import time
+from collections import UserDict
+from peewee import Field
+from peewee import Model
+
+
+class ModelsToDict(dict):
+    """peewee to dict
+    :type data: object
+    :type is_shield: bool
+    :type shield_field: list [peewee Field, "name"]
+    :type field_name: dict {filed or peewee Field: "name"}
+    :type field_handle: dict {filed or peewee Field: func}
+    :param data: peewee query object
+    :param is_shield: is shield kw filed
+    :param shield_field: shield filed
+    :param field_handle: apponit filed handle
+    :param field_name: apponit filed Modified alias
+    """
+
+    def __bool__(self):
+        if isinstance(self._data, list):
+            return bool(len(self._data))
+        if isinstance(self._data, dict):
+            return bool(self._data)
+
+    def __getitem__(self, item):
+        print(item)
+
+    def __init__(self, **kw):
+        self._data = kw['data']
+        self.is_shield = kw.get('is_shield', True)
+        self.field_name = self._kw_handle(kw.get('field_name', {}))
+        self.shield_field = self._kw_handle([] if kw.get('shield_field') is None else kw['shield_field'])
+        self.field_handle = self._kw_handle({} if kw.get('field_handle') is None else kw['field_handle'])
+        self._data = self._models_dict()
+        if isinstance(self._data, dict):
+            super(ModelsToDict, self).__init__(**self._data)
+
+    @property
+    def data(self):
+        return self._data
+
+    def _kw_handle(self, kw):
+        if isinstance(kw, list):
+            tmp_ = []
+            for i in kw:
+                if isinstance(i, Field):
+                    tmp_.append(i.name)
+                elif isinstance(i, str):
+                    tmp_append(i)
+                else:
+                    raise TypeError("{} not peewee Field or not str".format(i))
+            return tmp_
+        elif isinstance(kw, dict):
+            tmp_ = {}
+            for i in kw:
+                if isinstance(i, Field):
+                    tmp_[i.name] = kw[i]
+                elif isinstance(i, str):
+                    tmp_[i] = kw[i]
+                else:
+                    raise TypeError("{} not peewee Field or not str".format(i))
+            return tmp_
+        else:
+            raise TypeError("{} not list or not dict".fromat(kw))
+
+    def _models_dict(self):
+        if not self._data:
+            return {}
+        if hasattr(self._data, 'get_query_meta'):
+            model_fileds = self._data.get_query_meta()[0]
+            return_data = []
+            for _model in self._data:
+                tmp_dict = {}
+                for filed in model_fileds:
+                    if self.is_shield and filed.name in self.shield_field:
+                        # shield
+                        continue
+                    _ = getattr(_model, filed.name)
+                    _field_name = filed.name
+                    if filed.name in self.field_handle:
+                        _ = self.field_handle[filed.name](_)
+                    if filed.name in self.field_name:
+                        _field_name = self.field_name[_field_name]
+                    tmp_dict[_field_name] = _
+                return_data.append(tmp_dict)
+            return return_data
+
+        model_fileds = self._data.select().get_query_meta()[0]
+        return_data = {}
+        for filed in model_fileds:
+            if self.is_shield and filed.name in self.shield_field:
+                # shield
+                continue
+            _ = getattr(self._data, filed.name)
+            _field_name = filed.name
+            if filed.name in self.field_handle:
+                _ = self.field_handle[filed.name](_)
+            if filed.name in self.field_name:
+                _field_name = self.field_name[_field_name]
+            return_data[_field_name] = _
+        return return_data
+
+
 def models_to_dict(model):
     """peewee Model sequences into Dict
     :param model:peewee query model object
@@ -11,3 +116,5 @@ def models_to_dict(model):
         return [{l.name: getattr(i, l.name) for l in model_filed} for i in model]
     model_filed = model.select().get_query_meta()[0]
     return {l.name: getattr(model, l.name) for l in model_filed}
+
+
