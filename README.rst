d1a375b74ad91dd1724e7eacd70a800f403d9658
perror <15058342792@163.com>
2018-03-19 19:34:00 +0800

    dify the instructions that conform to the pypi standard and setup

--- README.rst
+++ README.rst
@@ -1,6 +1,7 @@
-=========
+####
 SpiderMan
-=========
+####
 SpiderMan Based on Scrapy, scrapyd, scrapy-API, tornado spider distributed management framework.
 
 Features include spider scheduling, Web side SSH, Web end code editor and Scrapy project construction and so on.
+Detailed content: https://github.com/QYLGitHub/SpiderMan/tree/develop
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 import os
 import SpiderMan
 
+
 def read_file():
     with open('README.rst', encoding='utf-8') as f:
         return f.read()
@@ -22,12 +23,16 @@ def package_files(dirs):
             [paths.append(os.path.join(_path[0], i)) for i in _path[2] if '.git' not in _path[0]]
     return paths
 
+
 setup(
     name='SpiderMan',
     version=SpiderMan.version(),
+    long_description=read_file(),
     description='Management of spiders based on scrapyd',
     keywords=['SpiderMan', 'scrapy', 'Distributed Management'],
     author='perror',
+    email='perror_develop@sina.com',
+    url="https://github.com/QYLGitHub/SpiderMan/tree/develop",
     install_requires=get_install_pack(),
     packages=find_packages(),
     entry_points={
@@ -45,5 +50,17 @@ setup(
         'sudo python3 setup.py sdist',
         'sudo python3 setup.py bdist_egg upload'
         'sudo python3 setup.py sdist upload'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache-2.0',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
